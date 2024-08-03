6d2bcee662533fb8bd925af4cc6fb93bfe286f4b
Shradha Chauhan <shradhachauhan31433@gmail.com>
2024-08-03 20:23:10 +0530

    Changed client folder location

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require('path');
 
 connectToMongo();
 
@@ -15,6 +16,12 @@ app.use('/api/notes', require('./routes/notes'));
 app.use('/api/users', require('./routes/users'));
 
 app.use('/api/auth', require('./routes/auth'));
+
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`notewhril backend listening on port ${PORT}`)
