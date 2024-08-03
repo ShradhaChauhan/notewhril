093a0ec9289de81c71c95f6926ea73055b92631b
Shradha Chauhan <shradhachauhan31433@gmail.com>
2024-08-03 20:01:13 +0530

    Final code
    mern app deployment

--- backend/index.js
+++ backend/index.js
@@ -16,14 +16,6 @@ app.use('/api/users', require('./routes/users'));
 
 app.use('/api/auth', require('./routes/auth'));
 
-if(process.env.NODE_ENV=="production"){
-  app.use(express.static("client/build"));
-  const path = require('path');
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-  })
-}
-
 app.listen(PORT, () => {
   console.log(`notewhril backend listening on port ${PORT}`)
 });
